6325f7679ee87652620d05ddd30d0a0b0a384f4e
seevuh <119629904+seevuh@users.noreply.github.com>
2026-03-05 13:27:44 +0530

    sum object properties

--- src/c4-objects.js
+++ src/c4-objects.js
@@ -7,3 +7,18 @@ function isEmpty(obj) {
     }
     return true;
 };
+
+function sumSalaries(salaries) {
+    let sum = 0;
+
+    for (let key in salaries) {
+        // console.log(+salaries[key]);
+        if (typeof salaries[key] === "number") {
+            sum += salaries[key];
+        }
+        // if we want to convert non-numeric values to 0, we can use the unary plus operator:
+        // sum += +salaries[key] || 0;
+    }
+
+    return sum;
+}
--- tests/c4-objects-tests.js
+++ tests/c4-objects-tests.js
@@ -10,3 +10,27 @@ describe("isEmpty", function() {
         assert.equal(isEmpty({a: 1}), false);
     });
 });
+
+describe("sum of salaries", function() {
+    it("should return 0 for an empty object {}", function() {
+        assert.equal(sumSalaries({}), 0);
+    });
+    
+    it("Should return the sum of salaries for an object with properties. Here 100 + 300 + 250 = 650", function() {
+        let salaries = {
+            "John": 100,
+            "Pete": 300,
+            "Mary": 250
+        };
+        assert.equal(sumSalaries(salaries), 650);
+    });
+
+    it("Should return the sum of salaries with a non-numeric value. Here 100 + 250 = 350", function() {
+        let salaries = {
+            "John": 100,
+            "Pete": "fd",
+            "Mary": 250
+        };
+        assert.equal(sumSalaries(salaries), 350);
+    });
+});
