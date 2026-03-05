041786ba5c9139c69828b4bc4bb5e7fdeb4eb406
seevuh <119629904+seevuh@users.noreply.github.com>
2026-03-05 12:51:25 +0530

    code quality tests

--- tests/code-quality-tests.js
+++ tests/code-quality-tests.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+"use strict";
+
 describe("pow", function() {
 
 /*     before( () => console.log("Testing started") );
@@ -7,5 +10,27 @@ describe("pow", function() {
 
     it("2 raised to the power of 3 is 8", function() {
         assert.equal(pow(2, 3), 8);
+    });
+
+    it("for negative n the result is NaN", function() {
+        assert.isNaN(pow(2, -1));
+    });
+
+    it("for non-integer n the result is NaN", function() {
+        assert.isNaN(pow(2, 1.5));
+    });
+
+    describe("raises x to the power of 3", function() {
+
+        function makeTest(x) {
+            let expected = x * x * x;
+            it(`${x} in the power of 3 is ${expected}`, function() {
+                assert.equal(pow(x, 3), expected);
+            });
+        }
+
+        for (let n = 1; n <= 5; n++) {
+            makeTest(n);
+        }
     });
 });
