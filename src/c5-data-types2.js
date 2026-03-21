16195e699f95ca95dfac96fdbc22b3e035f95053
seevuh <119629904+seevuh@users.noreply.github.com>
2026-03-21 12:22:27 +0530

    5.7 Map and Set

--- src/c5-data-types2.js
+++ src/c5-data-types2.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// #### 5.6 Iterables ####
+//#region #### 5.6 Iterables ####
 {
     console.log(`#### 5.6 Iterables ####\n\n`);
 
@@ -133,3 +133,217 @@
     // the native method does not support surrogate pairs
     console.log( str.slice(1, 3) ); // garbage
 }
+// #endregion
+
+// #region Map
+{
+    // Map
+    console.log('\n*** Map ***');
+    let map = new Map();
+
+    map.set('1', 'str1'); // a string key
+    map.set(1, 'num1'); // a numeric key
+    map.set(true, 'bool1'); // a boolean key
+
+    // remember the regular Object? it would convert keys to string
+    // Map keeps the type, so these two are different:
+    console.log( map.get(1) ); // 'num1'
+    console.log( map.get('1') ); // 'str1'
+
+    console.log( map.size ); // 3
+
+    console.log('\n*** Map can also use object as keys ***');
+    let john1 = { name: "John" };
+
+    // for every user, let's store their visits count
+    let visitsCountMap = new Map();
+
+    // john is the key for the map
+    visitsCountMap.set(john1, 123);
+
+    console.log( visitsCountMap.get(john1) ); // 123
+
+    // Object
+    console.log('\n*** visitsCountObj ***');
+    let john2 = { name: "John" };
+    let ben2 = { name: "Ben" };
+
+    let visitsCountObj = {};
+
+    visitsCountObj[ben2] = 234;
+    visitsCountObj[john2] = 123;
+
+    console.log( visitsCountObj["[object Object]"] );
+
+    // chaining
+    map.clear();
+    map.set('1', 'str1')
+       .set(1, 'num1')
+       .set(true, 'bool1');
+
+
+    // Iteration over Map
+    console.log('\n*** Iteration over Map ***');
+
+    let recipeMap = new Map();
+    recipeMap.set('cucumber', 500)
+             .set('tomatoes', 350)
+             .set('onion', 50);
+
+    let a = recipeMap.set('carrot', 100);
+    let b = recipeMap.get('carrot');
+    let c = recipeMap.has('carrot');
+    let d = recipeMap.delete('carrot');
+    let e = recipeMap.size;
+    // let f = recipeMap.clear();
+    console.log(a, b, c, d, e);
+
+    // iterate over keys (vegetables)
+    for (let vegetable of recipeMap.keys()) {
+        console.log( vegetable);  // cucumber, tomatoes, onion
+    }
+
+    // iterate over values (amounts)
+    for (let amount of recipeMap.values()) {
+        console.log(amount); // 500, 350, 50
+    }
+
+    // iterate over [key, value] entries
+    for (let entry of recipeMap) {
+        console.log( entry ); // cucumber, 500
+    }
+
+    // runs the function for each (key, value) pair
+    recipeMap.forEach( (value, key, map) => {
+        console.log(`${key}: ${value}`); // cucumber: 500 etc
+    })
+
+    // Object.entries: Map from Object
+    console.log('\n*** Object.entries: Map from Object ***');
+    // array of [key, value] pairs
+    let map1 = new Map([
+        ['1', 'str1'],
+        [1, 'num1'],
+        [true, 'bool1']
+    ]);
+
+    console.log( map.get('1') ); // str1
+
+    let obj = {
+        name: "John",
+        age: 30
+    };
+    let map2 = new Map( Object.entries(obj) );
+    console.log( map2.get('name') );
+
+
+    // Object.fromEntries: Object from Map
+    console.log('\n*** Object.fromEntries: Object from map ***');
+    let prices = Object.fromEntries([
+        ['banana', 1],
+        ['orange', 2],
+        ['meat', 4]
+    ]);
+
+    // now prices = { banana: 1, orange: 2, meat: 4 }
+    console.log(prices.orange); // 2
+
+    let map3 = new Map();
+    map3.set('banana', 1);
+    map3.set('orange', 2);
+    map3.set('meat', 4);
+
+
+    let obj2 = Object.fromEntries( map3.entries() ); // make a plain object (*)
+
+    console.log( obj2.orange );
+
+    let obj3 = Object.fromEntries(map3);
+    console.log(obj3);
+
+
+    // Set
+    console.log('\n*** Set ***');
+
+    let set = new Set();
+
+    let john = { name: "John" };
+    let pete = { name: "Pete" };
+    let mary = { name: "Mary" };
+
+    // visits, some users come multiple times
+    set.add(john);
+    set.add(pete);
+    set.add(mary);
+    set.add(john);
+    set.add(mary);
+
+    // set keeps only unique values
+    console.log( set.size ); // 3
+
+    for (let user of set) {
+        console.log( user.name ); 
+    }
+
+    // Iteration over Set
+    console.log('\n*** Iteration over Set ***');
+
+    let set2 = new Set(["oranges", "apples", "bananas"]);
+
+    for (let value of set2 ) console.log(value);
+
+    set2.forEach((value, valueAgain, set) => {
+        console.log(value, set);
+    });
+
+    // Tasks
+    console.log('\n*** Tasks ***');
+    {
+        // 1
+        function unique(arr) {
+            return Array.from(new Set(arr));
+        }
+
+        let values = ["Hare", "Krishna", "Hare", "Krishna",
+            "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+        
+        console.log( unique(values) );
+
+        // 2
+        function aclean(arr) {
+            let map = new Map();
+            for (let word of arr) {
+                map.set(word.toLowerCase().split('').sort().join(''), word);
+            }
+            return Array.from(map.values());
+        }
+
+        function acleanObj(arr) {
+            let obj = {};
+            for (let word of arr) {
+                let sorted = word.toLowerCase()
+                                 .split('')
+                                 .sort()
+                                 .join('');
+                obj[sorted] = sorted;
+            }
+            return Object.values(obj);
+        }
+
+        let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+        console.log( aclean(arr) );
+        console.log( acleanObj(arr) );
+
+        // 3
+
+        let map = new Map();
+        map.set("name", "John");
+
+        let keys = Array.from(map.keys());
+        
+        keys.push("more");
+        
+        console.log(keys);
+    }
+}
+// #endregion
