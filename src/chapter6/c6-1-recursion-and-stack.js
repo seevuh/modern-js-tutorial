e69603ea82b0933c117c87ef9bb1d4b423a35a12
seevuh <119629904+seevuh@users.noreply.github.com>
2026-04-10 21:06:54 +0530

    chapter 6.1: tasks

--- src/chapter6/c6-1-recursion-and-stack.js
+++ src/chapter6/c6-1-recursion-and-stack.js
@@ -64,4 +64,113 @@ function sumTo(n) {
   return n * (n + 1) / 2;
 }
 
+// Task 2: Calculate factorial
+function factorial(n) {
+  return n == 0 ? 1 : n * factorial(n - 1);
+  // return n ? n * factorial(n - 1) : 1;
+}
+
+// Task 3: Fibonacci numbers
+function fib1(n) {
+  if (n == 1 || n == 2) return 1;
+  return fib1(n - 1) + fib1(n - 2);
+}
+
+function fib(n) {
+  if (n == 1 || n == 2) return 1;
+
+  let fnMinusOne = 1;
+  let fnMinusTwo = 1;
+  for (let i = 3; i <= n; i++) {
+    // fn = fn-1 + fn-2; (but saving it to fn-1 for next iteration i.e, fn+1)
+    [fnMinusOne, fnMinusTwo] = [fnMinusOne + fnMinusTwo, fnMinusOne]
+  }
+  return fnMinusOne;
+}
+
+// Task 4: Output a single-linked list
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+// Hard coded to length 4: wrong for linked list above 4
+function printList1(list) {
+  for (let i = 0, next = ''; i < 4; i++, next += '.next') {
+    console.log(eval(`list${next}.value`));
+  }
+}
+
+// I didn't think of while -- EASY & IMPORTANT
+function printList(list) {
+  let tmp = list;
+
+  while (tmp) {
+    console.log(tmp.value);
+    tmp = tmp.next;
+  }
+}
+
+function printList2(list) {
+  console.log(list.value);
+  if (list.next) printList(list.next);
+}
+
+printList(list);
+
+// Task 5: Output a single-linked list in the reverse order
+
+// thought I need actual reverse linked list but just printing..
+// so initially wrote to do that but can't do with recursion..
+// also since object is by reference, it will modify the original
+function reverseList(list) {
+  let reverseList = list;
+  let remainingList = list.next;
+  reverseList.next = null;
+
+  while (remainingList) {
+    let node = remainingList;
+    remainingList = remainingList.next;
+    node.next = reverseList;
+    reverseList = node;
+  }
+  return reverseList;
+}
+
+// solution on site
+function printReverseList1(list) {
+  let arr = [];
+  let tmp = list;
+
+  while (tmp) {
+    arr.push(tmp.value);
+    tmp = tmp.next;
+  }
+
+  for (let i = arr.length -1; i >= 0; i--) {
+    console.log( arr[i] );
+  }
+}
+
+printReverseList1(list);
+
+// solution on site
+function printReverseList(list) {
+  if (list.next) {
+    printReverseList(list.next);
+  }
+  console.log(list.value);
+}
+
+printReverseList(list);
+
 // #endregion
--- tests/chapter6/c6-1-recursion-and-stack.test.js
+++ tests/chapter6/c6-1-recursion-and-stack.test.js
@@ -5,19 +5,45 @@ describe("sumTo(n)", function () {
     assert.equal(sumTo(1), 1);
   });
 
-  it("should return 3 for n = 2", function() {
+  it("should return 3 for n = 2", function () {
     assert.equal(sumTo(2), 3);
   });
 
-  it("should return 6 for n = 3", function() {
+  it("should return 6 for n = 3", function () {
     assert.equal(sumTo(3), 6);
   });
 
-  it("should return 10 for n = 4", function() {
+  it("should return 10 for n = 4", function () {
     assert.equal(sumTo(4), 10);
   });
 
-  it("should return 5050 for n = 100", function() {
+  it("should return 5050 for n = 100", function () {
     assert.equal(sumTo(100), 5050);
   });
+});
+
+describe("factorial(n)", function () {
+  let nFactorial = [1, 1, 2, 6, 24, 120];
+
+  for (let i = 0; i <= 5; i++) {
+    it(`should return ${nFactorial[i]} for n = ${i}`, function () {
+      assert.equal(factorial(i), nFactorial[i]);
+    });
+  }
+});
+
+describe("Fibonacci number: n-th, fib(n)", function () {
+  let nFib = { 3: 2, 4: 3, 5: 5, 6: 8, 7: 13, 77: 5527939700884757 };
+
+  for (let [n, value] of Object.entries(nFib)) {
+    it(`should return ${value} for n = ${n}`, function () {
+      assert.equal(fib(n), value);
+    });
+  }
+
+  // for (let key in nFib) {
+  //   it(`should return ${nFib[key]} for n = ${key}`, function () {
+  //     assert.equal(fib(key), nFib[key]);
+  //   });
+  // }
 });
